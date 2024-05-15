b78d9c8ca3df864baed6d4af1432b66ddd269c8a
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-15 13:25:58 -0300

    Creacion del proyecto

---
+++ src/test/java/com/ramitax/SistemaDeGestion/SistemaDeGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramitax.SistemaDeGestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
