25f26e1db2a6fde17d389815ba7aaddaaa67389a
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-15 15:31:48 -0300

    Genera campos, metodos get/set y constructores de Usuarios

--- src/main/java/com/ramitax/model/Usuario.java
+++ src/main/java/com/ramitax/model/Usuario.java
@@ -1,5 +1,22 @@
 package com.ramitax.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private Estado estado;
 
 }
