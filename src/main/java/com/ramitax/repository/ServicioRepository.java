47f3fe74c8d849e5fe5f09892e84fa0ec65788b4
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-21 11:54:03 -0300

    Creacion del repositorio para servicios

---
+++ src/main/java/com/ramitax/repository/ServicioRepository.java
@@ -0,0 +1,9 @@
+package com.ramitax.repository;
+
+import com.ramitax.model.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicioRepository extends JpaRepository<Servicio, Integer> {
+}
