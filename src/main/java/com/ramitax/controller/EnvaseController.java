e76b7cf6daa2f4a1a5505be47d30ba7549c80123
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-21 16:12:31 -0300

    Definicion de metodos en el controlador

--- src/main/java/com/ramitax/controller/EnvaseController.java
+++ src/main/java/com/ramitax/controller/EnvaseController.java
@@ -1,10 +1,42 @@
 package com.ramitax.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ramitax.model.Envase;
+import com.ramitax.service.EnvaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/envases")
 public class EnvaseController {
 
+    @Autowired
+    private EnvaseService envaseService;
+
+    @GetMapping(value = "/")
+    public @ResponseBody List<Envase> getAllEnvases(){
+        return envaseService.getAllEnvases();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Optional<Envase> getEnvaseById(@PathVariable("id") Integer id){
+        return envaseService.getEnvaseById(id);
+    }
+
+    @PostMapping(value="/", consumes = "application/json")
+    public @ResponseBody Envase addEnvase(@RequestBody Envase envase){
+        return envaseService.addEnvase(envase);
+    }
+
+    @PutMapping(path = "/{id}", consumes = "application/json")
+    public @ResponseBody Envase updateEnvase(@RequestBody Envase envase){
+        return envaseService.updateEnvase(envase);
+    }
+
+    @DeleteMapping(path = "/{id}", produces = "application/json")
+    public @ResponseBody void deleteEnvase(@PathVariable("id") Integer id){
+         envaseService.deleteEnvase(id);
+    }
 }
