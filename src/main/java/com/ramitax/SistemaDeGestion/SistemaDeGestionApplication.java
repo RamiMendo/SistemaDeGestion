cd9eb405ca0eaf6f3a60effb0497a0aaa0716551
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-21 13:22:57 -0300

    Añade bean de Open API para la documentacion en Swagger

--- src/main/java/com/ramitax/SistemaDeGestion/SistemaDeGestionApplication.java
+++ src/main/java/com/ramitax/SistemaDeGestion/SistemaDeGestionApplication.java
@@ -2,6 +2,10 @@ package com.ramitax.SistemaDeGestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class SistemaDeGestionApplication {
@@ -10,4 +14,13 @@ public class SistemaDeGestionApplication {
 		SpringApplication.run(SistemaDeGestionApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("V1") String AppVersion) {
+		return new OpenAPI().info(new Info().title("API Centro Optico Marcos")
+				.version(AppVersion)
+				.description("Centro Optico Marcos")
+				.termsOfService("http://google.com")
+				.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")));
+
+	}
 }
