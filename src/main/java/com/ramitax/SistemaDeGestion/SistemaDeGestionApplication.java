4611b7b322d4d6358f5b9f6c0402d0134d1ead33
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-25 12:38:43 -0300

    Eliminacion de Bean Open API en la clase Main

--- src/main/java/com/ramitax/SistemaDeGestion/SistemaDeGestionApplication.java
+++ src/main/java/com/ramitax/SistemaDeGestion/SistemaDeGestionApplication.java
@@ -1,13 +1,8 @@
 package com.ramitax.SistemaDeGestion;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -21,13 +16,4 @@ public class SistemaDeGestionApplication {
 		SpringApplication.run(SistemaDeGestionApplication.class, args);
 	}
 
-	@Bean
-	public OpenAPI customOpenAPI(@Value("V1") String AppVersion) {
-		return new OpenAPI().info(new Info().title("API Centro Optico Marcos")
-				.version(AppVersion)
-				.description("Centro Optico Marcos")
-				.termsOfService("http://google.com")
-				.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")));
-
-	}
 }
