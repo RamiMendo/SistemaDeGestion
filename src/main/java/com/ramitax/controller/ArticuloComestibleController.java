e1fe5f73a5c435dcf08fcc97fc11311255f229c3
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-28 09:41:29 -0300

    Agrega metodos al controlador de articulos comestibles

--- src/main/java/com/ramitax/controller/ArticuloComestibleController.java
+++ src/main/java/com/ramitax/controller/ArticuloComestibleController.java
@@ -2,6 +2,7 @@ package com.ramitax.controller;
 
 import com.ramitax.model.Comestible;
 import com.ramitax.service.ArticuloComestibleService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +15,15 @@ public class ArticuloComestibleController {
     @Autowired
     private ArticuloComestibleService articuloComestibleService;
 
-    @PostMapping(value = "/", consumes = "application/json")
+    @PostMapping(consumes = "application/json")
+    @Operation(summary = "Agrega un articulo comestible", description = "Debe enviar el articulo a ingresar", tags = {"Comestibles"})
     private @ResponseBody void addArticuloComestible(@RequestBody Comestible comestible){
         articuloComestibleService.addArticulosComestible(comestible);
     }
 
-//    @GetMapping(value="/", produces = "application/json")
-//    private @ResponseBody List<>
+    @GetMapping(produces = "application/json")
+    @Operation(summary = "Devuelve un listado de todos los articulos comestibles", description = "", tags = {"Comestibles"})
+    private @ResponseBody List<Comestible> findAllComestibles(){
+        return articuloComestibleService.findAllArticulosComestibles();
+    }
 }
